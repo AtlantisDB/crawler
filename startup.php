6478cddf3124d1c10539d36896ed6df5b2d32c64
Anthony Rossbach <me@anthonys.space>
2020-01-17 13:24:36 -0800

    Fixes to restart

--- startup.php
+++ startup.php
@@ -3,8 +3,8 @@ echo ++$argv[1];
 $_ = $_SERVER['_'];
 
 register_shutdown_function(function () {
-    global $_, $argv;
-    pcntl_exec($_, $argv);
+  #global $_, $argv;
+  #pcntl_exec($_, $argv);
 });
 
 //Import settings and begin connect to mysql tables for the system
