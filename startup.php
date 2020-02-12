545fd9246065034d6841f72d417ada2cab6387cd
Anthony Rossbach <me@anthonys.space>
2020-02-12 14:04:56 -0800

    Tests

--- startup.php
+++ startup.php
@@ -10,14 +10,14 @@ if ($json_a === null) {
   die("Unable to decode and load data");
 }
 
-$sqdbsettings["table_index_server"]=$json_a["index"]["server"];
-$sqdbsettings["table_index_username"]=$json_a["index"]["username"];
-$sqdbsettings["table_index_password"]=$json_a["index"]["password"];
-$sqdbsettings["table_index_database"]=$json_a["index"]["database"];
+$sqdbsettings["table_index_server"]=$json_a[0]["index"]["server"];
+$sqdbsettings["table_index_username"]=$json_a[0]["index"]["username"];
+$sqdbsettings["table_index_password"]=$json_a[0]["index"]["password"];
+$sqdbsettings["table_index_database"]=$json_a[0]["index"]["database"];
 
-$sqdbsettings["table_save_server"]=$json_a["save"]["server"];
-$sqdbsettings["table_save_username"]=$json_a["save"]["username"];
-$sqdbsettings["table_save_password"]=$json_a["save"]["password"];
-$sqdbsettings["table_save_database"]=$json_a["save"]["database"];
+$sqdbsettings["table_save_server"]=$json_a[0]["save"]["server"];
+$sqdbsettings["table_save_username"]=$json_a[0]["save"]["username"];
+$sqdbsettings["table_save_password"]=$json_a[0]["save"]["password"];
+$sqdbsettings["table_save_database"]=$json_a[0]["save"]["database"];
 
 ?>
